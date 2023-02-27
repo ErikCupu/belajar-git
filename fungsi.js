78c2c6f7abc9e19494710bbac37c477ab2c1dec1
unknown <erikkazama92@gmail.com>
2023-02-27 15:50:52 +0700

    Merevisi commit perkalian

--- fungsi.js
+++ fungsi.js
@@ -2,6 +2,7 @@ const add = (a, b) => a + b;
 console.log(add(1,2));
 
 const substract = (a, b) => a - b;
+<<<<<<< HEAD
 console.log(substract(5,2));
 
 const multiply = (a,b) => a * b;
@@ -9,3 +10,6 @@ console.log(multiply(2*2));
 
 const divide = (a,b) => a * b;
 console.log(divide(4/2));
+=======
+console.log(substract(1,2));
+>>>>>>> parent of 7dbcb62 (menambahkan fitur perkalian)
