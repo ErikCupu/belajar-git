3ed30a33b7b096e33b1e3cca4fd206c4eacc337a
unknown <erikkazama92@gmail.com>
2023-02-27 19:34:59 +0700

    Perubahan baru

--- fungsi.js
+++ fungsi.js
@@ -2,14 +2,10 @@ const add = (a, b) => a + b;
 console.log(add(1,2));
 
 const substract = (a, b) => a - b;
-<<<<<<< HEAD
-console.log(substract(5,2));
+console.log(substract(3,5));
 
-const multiply = (a,b) => a * b;
-console.log(multiply(2*2));
+const multiplyNumber = (a,b) => a * b;
+console.log(multiplyNumber(2*2));
 
 const divide = (a,b) => a * b;
 console.log(divide(4/2));
-=======
-console.log(substract(1,2));
->>>>>>> parent of 7dbcb62 (menambahkan fitur perkalian)
